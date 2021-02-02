a3e96706ab0cc3830e9780f1a5bedb3211647bc1
NoahNua <NoahNua@hawaii.edu>
2021-02-02 11:37:39 -1000

    finished part 5

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -12,13 +12,14 @@ if goodGrade == "Iʻve been doing all my work"
 {
 print("I should be getting an A at this point")
 }
+//part 3
 else
 {
 print("I need to work harder in this class")
 }
 
 
-
+//part 4
 var gradeA = true
 if gradeA == true
 {
@@ -28,3 +29,40 @@ else
 {
 print("Iʻm not so sure about coding in swift")
 }
+
+//part 5
+
+//problem 1
+var x = 10
+var y = 1
+if x < y
+{
+    print("Numbers are Fun")
+}
+else
+{
+    print("I hate math")
+}
+
+//problem 2
+
+var goodBoy = "Good job son"
+var jobDone = "Good job son"
+if goodBoy == jobDone
+{
+    print(goodBoy + jobDone)
+}
+
+//problem 3
+let stocked = 1
+let empty = 0
+
+if stocked == empty
+{
+    print("Were full")
+}
+else
+{
+    print("were empty")
+}
+
